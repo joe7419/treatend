d94ccda3f8def6c02c42d5ebb0fad334e99b9bf2
zf yu <joe7419@gmail.com>
2020-09-19 23:31:15 +0900

    Added files and changes required for deployment to heroku

--- src/libs/database.js
+++ src/libs/database.js
@@ -1,14 +1,19 @@
 const mysql = require('mysql');
 
 function create_connection() {
-    const pool = mysql.createPool(conf)
-    pool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    const pool = mysql.createPool({
+        connectionLimit : 10,
+        host: 'us-cdbr-east-02.cleardb.comyour host',
+        user:'b3d9ccad29aa7e',
+        password:'b1817360',
+        database:'heroku_2ae8333a90f6be8',    
+      });
+       
+      pool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
         if (error) throw error;
         console.log('The solution is: ', results[0].solution);
       });
 }
 
-
-  
 
 module.exports = {create_connection}
