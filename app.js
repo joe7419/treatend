d774aedd562d4820609ad364b5926ad2ebb825a6
zf yu <joe7419@gmail.com>
2020-09-19 22:47:08 +0900

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 //MySQL连接断开时自动重连
-var mysql = require('mysql');
-var pool  = mysql.createPool({
-  connectionLimit : 10,
-  host: 'us-cdbr-east-02.cleardb.com',
-  user:'b3d9ccad29aa7e',
-  password:'b1817360',
-  database:'heroku_2ae8333a90f6be8'
-});
-pool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-    if (error) throw error;
-    console.log('The solution is: ', results[0].solution);
-  }); 
+const pool = mysql.createPool({
+    host: 'us-cdbr-east-02.cleardb.com',
+    user:'b3d9ccad29aa7e',
+    password:'b1817360',
+    database:'heroku_2ae8333a90f6be8'
+  });
+  
+  // ... later
+  pool.query('select 1 + 1', (err, rows) => { /* */ });
+
 
 
 
