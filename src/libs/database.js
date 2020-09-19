603f4ae84c00322724c9f878a7985e45951fd251
zf yu <joe7419@gmail.com>
2020-09-19 23:40:57 +0900

    Added files and changes required for deployment to heroku

--- src/libs/database.js
+++ src/libs/database.js
@@ -1,19 +1,33 @@
-const mysql = require('mysql');
+var mysql = require("mysql");
+var pool = mysql.createPool({
+  host: 'us-cdbr-east-02.cleardb.com',
+  user:'b3d9ccad29aa7e',
+  password:'b1817360',
+  database:'heroku_2ae8333a90f6be8',
+  connectionLimit : 10, // 可以自己設定
+});
 
-function create_connection() {
-    const pool = mysql.createPool({
-        connectionLimit : 10,
-        host: 'us-cdbr-east-02.cleardb.com',
-        user:'b3d9ccad29aa7e',
-        password:'b1817360',
-        database:'heroku_2ae8333a90f6be8',    
-      });
-       
-      pool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-        if (error) throw error;
-        console.log('The solution is: ', results[0].solution);
-      });
-}
+var create_connection = function(sql, options, callback) {
+    console.log(sql, options, callback);
+    if (typeof options === "function") {
+        callback = options;
+        options = undefined;
+    }
+    pool.getConnection(function(err, conn){
+        if (err) {
+            callback(err, null, null);
+        } else {
+            conn.create_connection(sql, options, function(err, results, fields){
+                // callback
+                callback(err, results, fields);
+            });
+            // release connection。
+            // 要注意的是，connection 的釋放需要在此 release，而不能在 callback 中 release
+            conn.release();
+        }
+    });
+};
+
 
 
 module.exports = {create_connection}
