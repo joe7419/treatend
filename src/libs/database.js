f3bdbb35ebebef1b42de4e6a4f0fe0b9d6c85641
zf yu <joe7419@gmail.com>
2020-09-19 23:08:01 +0900

    Added files and changes required for deployment to heroku

--- src/libs/database.js
+++ src/libs/database.js
@@ -2,14 +2,12 @@ const mysql = require('mysql');
 
 function create_connection(conf, callback) {
     const connection = mysql.createPool(conf)
-    connection.connect(function (err) {
-        if (err) {
-            console.error('error connecting: ' + err.stack);
-            process.kill(process.pid)
-        }
-    })
     callback(connection)
 }
 
-
+pool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results[0].solution);
+  });
+  
 module.exports = {create_connection}
